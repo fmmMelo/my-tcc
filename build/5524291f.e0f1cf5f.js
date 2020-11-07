0c794c873d4f4af996e78f3c04bde8c74992c239
fmmMelo <felipe.moura@estudante.ifms.edu.br>
2020-11-07 03:06:00 -0400

    add pasta /build

---
+++ build/5524291f.e0f1cf5f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{68:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Seja bem-vindo!","permalink":"/my-tcc/blog/welcome"}]}')}}]);
